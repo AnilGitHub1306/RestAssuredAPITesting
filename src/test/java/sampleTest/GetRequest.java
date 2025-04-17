2e36f20b9ecd0a0213d59e65f11bf366ba8a047f
AnilGitHub1306 <aniladhav324@gmail.com>
2025-04-17 21:29:32 +0530

    Commit - First commit

---
+++ src/test/java/sampleTest/GetRequest.java
@@ -0,0 +1,29 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetRequest {
+
+	@Test
+	public void getListOfAllUsers() {
+
+		RequestSpecification requestSpe = RestAssured.given();
+		Response response = requestSpe.when().get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.asPrettyString());
+		
+		System.out.println("getStatusCode : "+response.getStatusCode());
+		System.out.println("getStatusLine : "+response.getStatusLine());
+		System.out.println("getSessionId : " +response.getSessionId());
+		System.out.println("getTime : "+response.getTime());
+		System.out.println("getCookies : "+response.getCookies());
+		System.out.println("getHeaders : "+response.getHeaders());
+		System.out.println("getBody : "+response.getBody().asString());
+		
+		
+	}
+}
